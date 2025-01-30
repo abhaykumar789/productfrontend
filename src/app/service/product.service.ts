38ee2cd44979ba0dd2bd9f14c9fc25803acf2296
abhaykumar789 <abhaykumar06278@gmail.com>
2025-01-30 11:47:42 +0530

    second commit

---
+++ src/app/service/product.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from '../model/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  // baseUrl = "http://localhost:8080"
+  baseUrl = "https://productappbackend.internal.wonderfulmushroom-dc7458d0.eastus.azurecontainerapps.io:8081"
+
+  constructor(private httpClient: HttpClient) { }
+
+  public addProduct1(productObject: any) {
+    return this.httpClient.post<Product>(this.baseUrl + "/add", productObject);
+  }
+  
+  public getProductById(pid:any){
+    return this.httpClient.get<Product>(this.baseUrl + "/get/by/pid/"+ pid)
+  }
+
+  public getAllProduct(){
+    return this.httpClient.get<Product[]>(this.baseUrl + "/all")
+  }
+}
